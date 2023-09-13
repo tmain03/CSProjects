3a19fe09dcf09b65f2f4c4545a5c0c39a368f10b
Toben <tmain2003@gmail.com>
2023-09-13 13:57:44 -0700

    Scheme Folder

---
+++ scheme/questions.scm
@@ -0,0 +1,81 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  (define (helper index s)
+    (if (null? s)
+      nil
+    (cons (list index (car s))(helper (+ 1 index) (cdr s)))))
+  (helper 0 s)
+)
+  
+  ; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists S1 and S2 according to ORDERED? and return
+;; the merged lists.
+(define (merge ordered? s1 s2)
+  ; BEGIN PROBLEM 16
+  (cond ((null? s1) s2)
+    ((null? s2) s1)
+    ((ordered? (car s2) (car s1))
+      (cons (car s2) (merge ordered? s1 (cdr s2))))
+    (else
+    (cons (car s1) (merge ordered? (cdr s1) s2)))))
+  ; END PROBLEM 16
+
+;; Optional Problem
+
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN OPTIONAL PROBLEM
+         'replace-this-line
+         ; END OPTIONAL PROBLEM
+         )
+        ((quoted? expr)
+         ; BEGIN OPTIONAL PROBLEM
+         'replace-this-line
+         ; END OPTIONAL PROBLEM
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN OPTIONAL PROBLEM
+           'replace-this-line
+           ; END OPTIONAL PROBLEM
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN OPTIONAL PROBLEM
+           'replace-this-line
+           ; END OPTIONAL PROBLEM
+           ))
+        (else
+         ; BEGIN OPTIONAL PROBLEM
+         'replace-this-line
+         ; END OPTIONAL PROBLEM
+         )))
+
+; Some utility functions that you may find useful to implement for let-to-lambda
+
+(define (zip pairs)
+  'replace-this-line)
